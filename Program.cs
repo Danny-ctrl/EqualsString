fd9f35535dd26e921ed107d486d5f240d7ff2da7
Daniele Freitas <71996747+Danny-ctrl@users.noreply.github.com>
2022-03-26 20:08:02 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace EqualsString
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var texto = "Este texto é um teste";
+
+      System.Console.WriteLine(texto.Equals("Este texto é um teste"));
+      System.Console.WriteLine(texto.Equals("esse texto é um teste"));
+      System.Console.WriteLine(texto.Equals("este texto é um teste"));
+    }
+  }
+}
